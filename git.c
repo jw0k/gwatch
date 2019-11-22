9f8509134e034cb49bf69ab9b7c25ab57cb3467d
Jacek Kuczera <jacek.kuczera@gmail.com>
2019-11-22 17:29:50 +0100

    cleanup git related code

--- git.c
+++ git.c
@@ -40,8 +40,6 @@ bool check_if_valid_git_repo()
 
 int status_cb(const char* path, unsigned int status_flags, void* payload)
 {
-    (void)status_flags;
-
     git_index* index = (git_index*)payload;
 
     if (strcmp(path, get_prog_name()) != 0)
@@ -72,23 +70,19 @@ int status_cb(const char* path, unsigned int status_flags, void* payload)
     return 0;
 }
 
-void commit()
+void commit_impl(git_repository** repo, git_index** index, git_tree** tree,
+        git_signature** gwatch_sig, git_commit** parent)
 {
-    plog("Trying to commit...");
-
-    git_repository* repo = NULL;
-
-    if (check_error(git_repository_open(&repo, get_repo_path())))
+    if (check_error(git_repository_open(repo, get_repo_path())))
     {
         plog("Cannot open the git repository");
         return;
     }
 
-    int unborn = git_repository_head_unborn(repo);
+    int unborn = git_repository_head_unborn(*repo);
     if (check_error(unborn))
     {
         plog("Cannot check if HEAD is unborn");
-        git_repository_free(repo);
         return;
     }
     if (unborn)
@@ -96,78 +90,60 @@ void commit()
         plog("HEAD is unborn - creating initial commit...");
     }
 
-    int detached = git_repository_head_detached(repo);
+    int detached = git_repository_head_detached(*repo);
     if (check_error(detached))
     {
         plog("Cannot check if HEAD is detached");
-        git_repository_free(repo);
         return;
     }
     if (detached)
     {
         plog("HEAD is detached - will not commit");
-        git_repository_free(repo);
         return;
     }
 
-    git_index* index = NULL;
-    if (check_error(git_repository_index(&index, repo)))
+    if (check_error(git_repository_index(index, *repo)))
     {
         plog("Cannot open the index file");
-        git_repository_free(repo);
         return;
     }
 
     files_added = 0;
-    int status_result = git_status_foreach(repo, status_cb, index);
+    int status_result = git_status_foreach(*repo, status_cb, index);
     if (check_error(status_result))
     {
         plog("Cannot add files to index");
-        git_index_free(index);
-        git_repository_free(repo);
         return;
     }
     if (files_added == 0)
     {
-        plog("No changes - will not commit");
-        git_index_free(index);
-        git_repository_free(repo);
+        // let's avoid too many log messages
+        // plog("No changes - will not commit");
         return;
     }
-    if (check_error(git_index_write(index)))
+    if (check_error(git_index_write(*index)))
     {
         plog("Cannot write index to disk");
-        git_index_free(index);
-        git_repository_free(repo);
         return;
     }
 
     git_oid tree_id;
-    if (check_error(git_index_write_tree(&tree_id, index)))
+    if (check_error(git_index_write_tree(&tree_id, *index)))
     {
         plog("Cannot write index as a tree");
-        git_index_free(index);
-        git_repository_free(repo);
         return;
     }
 
-    git_tree* tree = NULL;
-    if (check_error(git_tree_lookup(&tree, repo, &tree_id)))
+    if (check_error(git_tree_lookup(tree, *repo, &tree_id)))
     {
         plog("Cannot find the index tree object");
-        git_index_free(index);
-        git_repository_free(repo);
         return;
     }
 
-    git_signature* gwatch_sig = NULL;
-    if (check_error(git_signature_now(&gwatch_sig,
+    if (check_error(git_signature_now(gwatch_sig,
                     "gwatch", "gwatch@example.com")))
     {
         plog("Cannot create the signature");
-        git_tree_free(tree);
-        git_index_free(index);
-        git_repository_free(repo);
         return;
     }
 
@@ -175,73 +151,69 @@ void commit()
     if (!unborn)
     {
         git_oid parent_id;
-        if (check_error(git_reference_name_to_id(&parent_id, repo, "HEAD")))
+        if (check_error(git_reference_name_to_id(&parent_id, *repo, "HEAD")))
         {
             plog("Cannot find HEAD id");
-            git_tree_free(tree);
-            git_index_free(index);
-            git_repository_free(repo);
             return;
         }
-        git_commit* parent = NULL;
-        if (check_error(git_commit_lookup(&parent, repo, &parent_id)))
+        if (check_error(git_commit_lookup(parent, *repo, &parent_id)))
         {
             plog("Cannot find HEAD commit");
-            git_tree_free(tree);
-            git_index_free(index);
-            git_repository_free(repo);
             return;
         }
 
         if (check_error(git_commit_create_v(
             &commit_id,
-            repo,
+            *repo,
             "HEAD",
-            gwatch_sig, // author
-            gwatch_sig, // committer
+            *gwatch_sig, // author
+            *gwatch_sig, // committer
             NULL, // utf-8 encoding
             "gwatch auto-commit",
-            tree,
+            *tree,
             1,
-            parent
+            *parent
         )))
         {
             plog("Cannot create a commit");
-            git_commit_free(parent);
-            git_tree_free(tree);
-            git_index_free(index);
-            git_repository_free(repo);
             return;
         }
-
-        git_commit_free(parent);
     }
     else
     {
         if (check_error(git_commit_create_v(
             &commit_id,
-            repo,
+            *repo,
             "HEAD",
-            gwatch_sig,
-            gwatch_sig,
+            *gwatch_sig,
+            *gwatch_sig,
             NULL,
             "gwatch auto-commit",
-            tree,
+            *tree,
             0
         )))
         {
             plog("Cannot create initial commit");
-            git_tree_free(tree);
-            git_index_free(index);
-            git_repository_free(repo);
             return;
         }
     }
 
+    plog("Successfully created a new commit");
+}
+
+void commit()
+{
+    git_repository* repo = NULL;
+    git_index* index = NULL;
+    git_tree* tree = NULL;
+    git_signature* gwatch_sig = NULL;
+    git_commit* parent = NULL;
+
+    commit_impl(&repo, &index, &tree, &gwatch_sig, &parent);
+
+    git_commit_free(parent);
     git_signature_free(gwatch_sig);
     git_tree_free(tree);
     git_index_free(index);
     git_repository_free(repo);
-
-    plog("Successfully created a new commit");
 }
